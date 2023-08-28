191af328fccb739880623ef674fb2ce205956970
iceman2146 <iceman2146@mail.ru>
2023-08-28 22:36:24 +0300

    f

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,43 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
+void print_bulk_cmd(std::ostream &out,
+                    const std::vector<std::string> &commands) {
+  if (commands.empty())
+    return;
 
-void print_bulk_cmd(std::ostream& out, const std::vector<std::string>& commands)
-{
-    if (commands.empty())
-        return;
+  out << "bulk:";
 
-    out<< "bulk:";
-
-    for (const auto &i : commands)
-    {
-        out <<i;
-        if(commands[commands.size()-1]!=i)
-            out<<',';
-        
-    }
+  for (const auto &i : commands) {
+    out << i;
+    if (commands[commands.size() - 1] != i)
+      out << ',';
+  }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-        std::string command;
-        std::vector<std::string> commands_block;
+  std::string command;
+  std::vector<std::string> commands_block;
 
-        
-        while(std::getline(std::cin, command))
-        {
-            if (command == "{")
-            {
-                
-                    print_bulk_cmd(std::cout, commands_block);
-                    commands_block.clear();
-                
-            }
-            else if (command == "}")
-            {
-                
-                    print_bulk_cmd(std::cout, commands_block);
-                    commands_block.clear();
-                
-            }
-            else
-            {
-                commands_block.push_back(command);
-            }
-        }
+  while (std::getline(std::cin, command)) {
+    if (command == "{") {
 
-    
-    print_bulk_cmd(std::cout, commands_block);
-    
-  
+      print_bulk_cmd(std::cout, commands_block);
+      commands_block.clear();
 
-    return 0;
+    } else if (command == "}") {
+
+      print_bulk_cmd(std::cout, commands_block);
+      commands_block.clear();
+
+    } else {
+      commands_block.push_back(command);
+    }
+  }
+
+  print_bulk_cmd(std::cout, commands_block);
+
+  return 0;
 }
